c2422ab380f7a049e6b556388921f570a548e334
antonyadhiban <gtadhib@gmail.com>
2018-06-15 19:47:04 +0530

    Flask error ongoing

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,11 @@
 import hashlib
 import json
 
+from textwrap import dedent 
 from time import time
 from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -11,6 +14,34 @@ class Blockchain(object):
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+        # Instantiate our Node
+        app = Flask(__name__)
+
+        # Generally a globally unique address for this node
+        node_identifier = str(uuid4()).replace('-', '')
+
+        # Instantiate the Blockchain
+        blockchain = Blockchain()
+
+        @app.route('/mine', methods=['GET'])
+        def mine():
+            return "We'll mine a new Block"
+
+        @app.route('/transactions/new', methods=['POST'])
+        def new_transaction():
+            return "We'll add a new transaction"
+
+        @app.route('/chain', methods=['GET'])
+        def full_chain():
+            response = {
+                'chain': blockchain.chain,
+                'length': len(blockchain.chain),
+            }
+            return jsonify(response), 200
+
+        if __name__ == '__main__':
+            app.run(host='0.0.0.0', port=5000)
 
     def new_block(self, proof, previous_hash=None):
         """
